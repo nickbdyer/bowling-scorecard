c1c8d70ac312acd400f5e940b0ebb6511cf26bf6
Nick Dyer <nbdyer@gmail.com>
2014-11-28 12:00:46 +0000

    Frame starts with 10 pins

---
+++ spec/BowlingSpec.js
@@ -0,0 +1,22 @@
+describe ('Bowling Scorecard', function() {
+
+  describe('Frame', function() {
+
+    it('should be instantiated with 10 pins', function(){
+      var frame = new Frame();
+      expect(frame.pinCount).toEqual(10);
+    });
+
+    
+
+
+
+
+
+  });
+
+
+
+
+
+});
---
+++ src/Bowling.js
@@ -0,0 +1,4 @@
+function Frame() {
+  this.pinCount = 10
+};
+
