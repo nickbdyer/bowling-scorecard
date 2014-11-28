13f50d61d985de31cf44d20007823043cef7c02d
Nick Dyer <nbdyer@gmail.com>
2014-11-28 12:26:11 +0000

    Frame only receives two throws

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,8 +7,20 @@ describe ('Bowling Scorecard', function() {
       expect(frame.pinCount).toEqual(10);
     });
 
-    
 
+    it('should receive a throw', function(){
+      var frame = new Frame();
+      frame.receiveShot(3);
+      expect(frame.pinCount).toEqual(7);
+    });
+
+
+    it('should only receive 2 throws', function() {
+      var frame = new Frame();
+      frame.receiveShot(2);
+      frame.receiveShot(2);
+      expect( function(){ frame.receiveShot(2);} ).toThrow(new Error("You can not receive another shot in this frame."))
+    });
 
 
 
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,4 +1,11 @@
 function Frame() {
   this.pinCount = 10
+  this.shotCount = 0
+};
+
+Frame.prototype.receiveShot = function(hitpins) {
+  if (this.shotCount === 2) { throw new Error ("You can not receive another shot in this frame."); }
+  this.pinCount -= hitpins;
+  this.shotCount++;
 };
 
