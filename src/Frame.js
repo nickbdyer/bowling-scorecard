4069d97011ea1798f0ea8b9e367a8c5bd138d3f0
Nick Dyer <nbdyer@gmail.com>
2014-12-01 08:31:54 +0000

    Refactor frame.js

--- src/Frame.js
+++ src/Frame.js
@@ -8,18 +8,6 @@ function Frame() {
   this.isFrame10 = false
 };
 
-Frame.prototype.receiveShot = function(hitpins) {
-  console.log(this)
-  if (this.isFrame10) {
-    this._checkShotLegalityFrame10(hitpins);
-    this._updateFrame10Stats(hitpins);
-    this.shotCount++;
-  } else { 
-    this._checkShotLegality(hitpins);
-    this._updateFrameStats(hitpins);
-    this.shotCount++;
-  };
-};
 
 Frame.prototype.isSpare = function() {
   if (((this.firstShot + this.secondShot) === 10) && this.secondShot !== null ) { return true };
@@ -29,35 +17,51 @@ Frame.prototype.isStrike = function() {
   if (this.firstShot === 10 ) { return true };
 };
 
-Frame.prototype._checkShotLegality = function(hitpins) {
-  if (this.shotCount === 2) { throw new Error ("You can not receive another shot in this frame."); }
-  if ((this.pinCount - hitpins) < 0 ) { throw new Error ("You can only hit 10 pins per frame.")}
-};
-
-Frame.prototype._updateFrameStats = function(hitpins) {
-  this.pinCount -= hitpins;
-  this.score += hitpins;
-  if (this.firstShot === null) { this.firstShot = hitpins 
-  } else { this.secondShot = hitpins };
-};
-
-Frame.prototype._checkShotLegalityFrame10 = function(hitpins) {
-  if (this.isSpare() && this.shotCount === 3) { throw new Error ("You can not receive another shot in this frame."); 
-  } else if (this.isStrike() && this.shotCount === 3) { throw new Error ("You can not receive another shot in this frame."); 
-  } else if ((!this.isStrike() && !this.isSpare()) && this.shotCount === 2) { throw new Error ("You can not receive another shot in this frame."); 
+Frame.prototype.receiveShot = function(hitpins) {
+  if (this.isFrame10) { this.receiveShotFrame10(hitpins);
+  } else { this.receiveShotRegularFrame(hitpins);
   };
 };
 
-Frame.prototype._updateFrame10Stats = function(hitpins) {
-  this._resetPins(hitpins);
-  this.score += hitpins;
-  if (this.firstShot === null) { this.firstShot = hitpins 
-  } else if ( this.secondShot === null) { this.secondShot = hitpins 
-  } else { this.thirdShot = hitpins};
+Frame.prototype.receiveShotRegularFrame = function(hitpins) {
+  this._checkShotLegality(hitpins);
+  this._updateFrameStats(hitpins);
+  this.shotCount++;
 };
 
-Frame.prototype._resetPins = function(hitpins) {
-  if (hitpins === 10 ) { this.pinCount = 10 
-  } else if ( this.firstShot + this.secondShot === 10 ) { this.pinCount = 10
-  } else { this.pinCount -= hitpins };
+    Frame.prototype._checkShotLegality = function(hitpins) {
+      if (this.shotCount === 2) { throw new Error ("You can not receive another shot in this frame."); }
+      if ((this.pinCount - hitpins) < 0 ) { throw new Error ("You can only hit 10 pins per frame.")}
+    };
+
+    Frame.prototype._updateFrameStats = function(hitpins) {
+      this.pinCount -= hitpins;
+      this.score += hitpins;
+      this.firstShot === null ? this.firstShot = hitpins : this.secondShot = hitpins;
+    };
+
+Frame.prototype.receiveShotFrame10 = function(hitpins) {
+  this._checkShotLegalityFrame10(hitpins);
+  this._updateFrame10Stats(hitpins);
+  this.shotCount++;
 };
+
+    Frame.prototype._checkShotLegalityFrame10 = function(hitpins) {
+      if (((this.isSpare() || this.isStrike()) && this.shotCount === 3) || ((!this.isStrike() && !this.isSpare()) && this.shotCount === 2)) { 
+        throw new Error ("You can not receive another shot in this frame."); 
+      };
+    };
+
+    Frame.prototype._updateFrame10Stats = function(hitpins) {
+      this._resetPins(hitpins);
+      this.score += hitpins;
+      if (this.firstShot === null) { this.firstShot = hitpins 
+      } else if ( this.secondShot === null) { this.secondShot = hitpins 
+      } else { this.thirdShot = hitpins };
+    };
+
+    Frame.prototype._resetPins = function(hitpins) {
+      if (hitpins === 10 ) { this.pinCount = 10 
+      } else if ( this.firstShot + this.secondShot === 10 ) { this.pinCount = 10
+      } else { this.pinCount -= hitpins };
+    };
